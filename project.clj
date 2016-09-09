3ee5e87c6ea504039a1bb2931af8683f589a4c4c
Duncan McGreggor <oubiwann@gmail.com>
2016-09-09 11:53:45 -0500

    Moved test plugins to test profile.

--- project.clj
+++ project.clj
@@ -17,13 +17,13 @@
     ;; Support & Util libs
     [clojusc/twig "0.2.3"]
     [potemkin "0.4.3"]]
-  :plugins [
-    [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-    [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]]
   :profiles {
     :uber {
       :aot :all}
     :test {
+      :plugins [
+        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]]
       :test-selectors {
       :default :unit
       :unit :unit
