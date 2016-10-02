ab988c114b44eef0fb566adfbd22dae82f49e27d
Duncan McGreggor <oubiwann@gmail.com>
2016-10-02 13:14:30 -0500

    Added starter for example.

---
+++ examples/tutorials/01-read-scenario/test/read_scenario/core_test.clj
@@ -0,0 +1,7 @@
+(ns read-scenario.core-test
+  (:require [clojure.test :refer :all]
+            [read-scenario.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
